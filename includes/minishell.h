e0b96d7925a1ae85294f8109852cce6d11279baf
zhy2on <dhwlgus5940@gmail.com>
2022-03-08 18:51:48 +0900

    Feat: basic command execute

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 21:40:49 by jihoh             #+#    #+#             */
-/*   Updated: 2022/03/07 19:14:37 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/03/08 17:06:26 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
-# define MAX_SIZE_CMD 256
-# define MAX_SIZE_ARG 16
+# define MAX_CMD 256
+# define MAX_LINE 1024
 
 typedef struct s_vars
 {
 	pid_t	pid;
+	char	cmd[MAX_CMD];
+	char	cwd[MAX_LINE];
 }				t_vars;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,29 +6,50 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 21:41:26 by jihoh             #+#    #+#             */
-/*   Updated: 2022/03/07 22:39:31 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/03/08 18:50:07 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_vars	g_vars;
-
-void	do_child(void)
+void	prompt(t_vars *vars, char **av, char **env)
 {
-	printf("child\n");
+	char	*str;
+	char	*args[2];
+
+	while (1)
+	{
+		str = readline("🐚minishell$ ");
+		if (!ft_strcmp(str, "exit"))
+			break ;
+		else if (!ft_strcmp(str, "pwd"))
+			printf("%s\n", getcwd(vars->cwd, MAX_LINE));
+		else
+		{
+			args[0] = str;
+			args[1] = NULL;
+			vars->pid = fork();
+			if (vars->pid == 0)
+			{
+				if (execvp(args[0], args) == -1)
+				{
+					printf("%s: command not found\n", str);
+					exit(0);
+				}
+			}
+			else
+				waitpid(vars->pid, NULL, 0);
+		}
+		free(str);
+	}
 	exit(0);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	pid_t	pid;
-	char	*ptr;
+	t_vars	vars;
+	int		status;
 
-	pid = fork();
-	if (pid == 0)
-		do_child();
-	else
-		printf("parent\n");
+	prompt(&vars, av, env);
 	return (0);
 }
